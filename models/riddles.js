3c04b48bda973ff6d4383b21cf86619930746414
LauraJohnston20 <95102375+LauraJohnston20@users.noreply.github.com>
2022-06-15 09:55:01 -0230

    first commit

---
+++ models/riddles.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const RiddleSchema = new mongoose.Schema({
+    riddle: {
+        type: String,
+        required: true,
+    },
+    solution: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+const RiddleModel = mongoose.model("riddles", RiddleSchema);
+module.exports = RiddleModel;
